a8023e531d9128e948f7447d00cb9af83c9fa718
Petya <145002477+raste9@users.noreply.github.com>
2023-12-05 11:03:14 +0200

    Update filter_word.py

--- filter_word.py
+++ filter_word.py
@@ -5,10 +5,3 @@ for word in ban_list_words:
     text = text.replace(word, '*' * len(word))
 
 print(text)
-
-# Linux, Windows
-# It is not Linux, it is GNU/Linux. Linux is
-# merely the kernel, while GNU adds the
-# functionality. Therefore we owe it to them
-# by calling the OS GNU/Linux! Sincerely, a
-# Windows client
